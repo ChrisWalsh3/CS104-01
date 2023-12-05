f0050d3cb929d64d60170a14309a63672e8f1eba
ChrisWalsh3 <146225357+ChrisWalsh3@users.noreply.github.com>
2023-12-04 22:19:48 -0500

    PythonQueue

---
+++ PythonQueue.py
@@ -0,0 +1,10 @@
+names = []
+for i in range(10):
+    name = input("Joe, Sally, Jim, June, Betty, Bill, Sara, Zak, Anne, Kate: ")
+    names.append(name)
+
+    print(names)
+
+    for i in range(10):
+        print(names[i])
+        print(i)
